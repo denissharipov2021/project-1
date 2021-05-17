9d519df7244fe3c557921951d4b49c9979541bd6
Denis Sharipov <boxdeniss1@yandex.ru>
2021-05-18 00:48:19 +0500

    add js-test file

---
+++ js.js
@@ -0,0 +1,26 @@
+let num = 50;
+if (num < 49) {
+    console.log("неверно!")
+} else if (num > 100) {
+    console.log('много!')
+} else {
+    console.log('верно!')
+}
+
+(num = 50) ? console.log('верно'): console.log('неверно');
+
+switch (num) {
+    case num < 49:
+        console.log('неверно');
+        break;
+    case num > 100:
+        console.log('много!');
+        break;
+    case 50:
+        console.log('верно');
+        break;
+    default:
+        console.log('Что то пошло не так!')
+        break;
+
+}
